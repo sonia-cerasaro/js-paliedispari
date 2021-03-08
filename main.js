02bef8e8332068a56949edb2b1819af27f04cff5
sonia-cerasaro <sonia.cerasaro@gmail.com>
2021-03-08 18:28:57 +0100

    palindroma completa

--- main.js
+++ main.js
@@ -1,24 +1,16 @@
-// come capisco che e' una parola palindroma?
-// Puo' essere pari e dispara.
-// Lettere e vocali sono identiche simmetricamente SE e' para.
-// SE INVECE e' dispara, SOLO le lettere piu' esterne saranno identiche, mentre quella interna
-// sara' diversa.
+var inputUtente = prompt("dammi una parola");
+var isPalindromo = palindroma(inputUtente);
 
-
-
-var input_utente = prompt("dammi una parola");
-var parola_utente = palindroma(input_utente);
-
-if (palindroma(parola_utente)) {
+if (isPalindromo) {
   console.log("la parola e' palindroma");
 } else {
-  console.log("la parola non e' palindroma");
+  console.log("la parola NON e' palindroma!!!");
 }
 
-function palindroma(input_utente) {
-  var lunghezza_parola = input_utente.length;
-  for (var i = 0; i < lunghezza_parola / 2; i++) {
-    if (input_utente.charAt(i) !== input_utente.charAt(lunghezza_parola - 1 - i)) {
+function palindroma(inputUtente) {
+  var lunghezzaParola = inputUtente.length;
+  for (var i = 0; i < lunghezzaParola / 2; i++) {
+    if (inputUtente.charAt(i) !== inputUtente.charAt((lunghezzaParola - 1)- i)) {
       return false;
     }
   }
